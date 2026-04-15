5821f3daec8603435982cb4314ef330e71eba783
Bruno Henrique Matias Barchesi <mac-bruno@MacBook-Air-de-Bruno.local>
2026-04-15 14:07:22 -0300

    adicionadas classes de security

---
+++ src/main/java/infrastucture/repository/TelefoneRepository.java
@@ -0,0 +1,10 @@
+package infrastucture.repository;
+
+import infrastucture.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> { //Aqui digo que é do tipo ENTITY telefoneO
+    //E QUE O ID É LONG
+}
